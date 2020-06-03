df1ce4565a77c0f0f7fc6799969b001a209473e2
= <navneil2006@gmail.com>
2020-06-03 21:12:08 +1000

    regulating the frame rate

--- src/main.cpp
+++ src/main.cpp
@@ -17,18 +17,28 @@ void render() {
 		ball->y,
 		ball->w,
 		ball->h
-	};
+	}; // This is the Ball Destination Rect
 
+	// Set The Color to White, To render the ball
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_RenderFillRect(renderer, &ballDstRect);
 }
 int main(int argc, char* argv[]) {
+	SDL_Init(SDL_INIT_EVERYTHING); // Initialise the SDL2 Context
+
 	window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, ScreenWidth, ScreenHeight, SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, 0);
 
 	SDL_Event e;
 	ball = new Ball(20, 20, 15, 15);
+
+	Uint32 CurrentFrame, FrameTime;
+	const int FPS = 30;
+	const int FrameDelay = 1000 / FPS;
+	
 	while (true) {
+		CurrentFrame = SDL_GetTicks();
+
 		SDL_PollEvent(&e);
 		if (e.type == SDL_QUIT)
 			break;
@@ -37,7 +47,12 @@ int main(int argc, char* argv[]) {
 		SDL_RenderClear(renderer);
 		render();
 		SDL_RenderPresent(renderer);
+
+		FrameTime = SDL_GetTicks() - CurrentFrame;
+		if (FrameDelay > FrameTime)
+			SDL_Delay(FrameDelay - FrameTime);
 	}
 
+	delete ball; // De-Allocate the Memory
 	return 0;
 }
